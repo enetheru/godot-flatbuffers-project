42bbe961fb1c2ebc9b57f1ba2b0c3dbe83853ff0
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-12-07 14:27:40 +1030

    ditch presets for now.

--- options.cmake
+++ options.cmake
@@ -8,14 +8,12 @@ set( BUILD_EXTENSION ON CACHE STRING "build the extension" )
 set( GDE_NAME "gdflatbuffers" CACHE STRING "The name of the extension library" )
 set( GDE_SUFFIX "" CACHE STRING "an additional suffix you can append to builds" )
 set( GDE_OUTPUT_NAME "" CACHE STRING "A custom output name, resulting binary will be: GDE_NAME.{editor|template_{release|debug}}[.GDE_SUFFIX].dll" )
-set( GDE_TARGETS "editor" CACHE STRING "The list of target configurations to build" )
-set_property( CACHE GDE_TARGETS PROPERTY STRINGS "template_release;template_debug;editor" )
 
 ### Information regarding the godot executable
 set( GODOT_EXECUTABLE "" CACHE FILEPATH "Path to the godot executable you are targeting" )
 set( GODOT_PROJECT_PATH "${PROJECT_SOURCE_DIR}/project" CACHE PATH "Path to a demo project that can test the gdextension" )
 
 ### godot-cpp
-set( GODOTCPP_GIT_URL "https://github.com/godotengine/godot-cpp.git" CACHE STRING "Location of the godot-cpp git respository" )
-set( GODOTCPP_GIT_TAG "" CACHE STRING "The git tag to use when pulling godot-cpp, will try to automatically detect based on godot.exe --version" )
+set( GODOTCPP_GIT_URL "https://github.com/enetheru/godot-cpp.git" CACHE STRING "Location of the godot-cpp git respository" )
+set( GODOTCPP_GIT_TAG "4.3-modernise" CACHE STRING "The git tag to use when pulling godot-cpp, will try to automatically detect based on godot.exe --version" )
 set( GODOTCPP_DIR "${PROJECT_SOURCE_DIR}/lib/godot-cpp" CACHE PATH "Path to the directory containing the godot-cpp GDExtension library, if we're fetching then this is where it will go" )
