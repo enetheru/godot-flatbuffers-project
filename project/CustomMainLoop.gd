37d1249195b9044e4c5f27c84337af9f62b39693
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-05-26 01:22:09 +0930

    update main scene readme.

--- project/CustomMainLoop.gd
+++ project/CustomMainLoop.gd
@@ -5,9 +5,10 @@ func _initialize():
 	print("_initialize()")
 	print( "LoadedExtensions: ", GDExtensionManager.get_loaded_extensions() )
 
-func _process(delta: float) -> bool:
+func _process(_delta: float) -> bool:
 	print("_process()")
-	return true
+	# Return true to end the main loop.
+	return Input.get_mouse_button_mask() != 0 || Input.is_key_pressed(KEY_ESCAPE)
 
 func _finalize():
 	print("_finalize()")
